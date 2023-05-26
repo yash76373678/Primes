c1ba22ed84d708d7964451a50f42f981b15dd105
vachhaniyash009 <vachhaniyash009@gmail.com>
2023-05-26 14:07:44 +0530

    Hii

---
+++ primes.py
@@ -0,0 +1,14 @@
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+# Example usage
+num = 17
+if is_prime(num):
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is not a prime number.")
